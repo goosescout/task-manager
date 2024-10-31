a523096ddba89f452091dc57f9e77859ddc7181e
GarnetAki <RomeNightingale@gmail.com>
2024-10-31 21:14:31 +0300

    fix search

--- src/main/kotlin/ru/quipy/repositories/UserRepository.kt
+++ src/main/kotlin/ru/quipy/repositories/UserRepository.kt
@@ -3,13 +3,11 @@ package ru.quipy.repositories
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import org.springframework.stereotype.Service
 import ru.quipy.entities.UserEntity
 
-@Service
 interface UsersRepository : JpaRepository<UserEntity, Long> {
     fun save(user: UserEntity)
 
-    @Query("SELECT * FROM Users WHERE name like :substr or login like :substr")
+    @Query("SELECT u FROM UserEntity u WHERE name like :substr or login like :substr")
     fun findAllBySubstr(@Param("substr")substr: String) : List<UserEntity>
 }
