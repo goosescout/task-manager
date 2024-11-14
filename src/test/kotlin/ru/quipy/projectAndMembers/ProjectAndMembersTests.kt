e494e5ca0dc0dc1e22a0ddb9b053089de60cd8ef
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-11-15 00:52:24 +0300

    test: refactor project tests

--- src/test/kotlin/ru/quipy/projectAndMembers/ProjectAndMembersTests.kt
+++ src/test/kotlin/ru/quipy/projectAndMembers/ProjectAndMembersTests.kt
@@ -1,6 +1,7 @@
 package ru.quipy.projectAndMembers
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -19,51 +20,145 @@ class ProjectAndMembersTests {
 	private lateinit var projectController: ProjectAndMembersController
 
 	@Test
-	fun createProjectHappyPath() {
+	fun testCreateProject() {
 		val owner = createUser("Owner")
-		val user = createUser("Member")
 		val project = projectController.createProject(
 			"testProject",
 			owner.userId
 		)
 		Assertions.assertEquals(1, project.version)
 		Assertions.assertEquals("testProject", project.projectName)
+	}
 
+	@Test
+	fun testGetProject() {
+		val owner = createUser("Owner")
+		val project = projectController.createProject(
+			"testProject",
+			owner.userId
+		)
 		val response = projectController.getProject(project.projectId)
 		Assertions.assertNotNull(response)
+		Assertions.assertEquals("testProject", response!!.getName())
+	}
 
-		val ownerInProject = response!!.getMemberById(response.getMembers()[0].id)
-		Assertions.assertNotNull(ownerInProject)
-		Assertions.assertEquals("testProject", response.getName())
-		Assertions.assertEquals("Owner", ownerInProject!!.name)
-		Assertions.assertEquals("LoginOwner", ownerInProject.login)
-		Assertions.assertEquals(response.getId(), ownerInProject.projectId)
-
+	@Test
+	fun testCreateMember() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
+		val project = projectController.createProject(
+			"testProject",
+			owner.userId
+		)
 		val newMember = projectController.createMember(
-			response.getId(),
+			project.projectId,
 			user.userId
 		)
-		Assertions.assertNotNull(ownerInProject)
+		Assertions.assertNotNull(newMember)
 		Assertions.assertEquals("LoginMember", newMember.memberLogin)
 		Assertions.assertEquals("Member", newMember.memberName)
 		Assertions.assertEquals(user.userId, newMember.userId)
-		Assertions.assertEquals(response.getId(), newMember.projectId)
+		Assertions.assertEquals(project.projectId, newMember.projectId)
+	}
 
+	@Test
+	fun testCreateInvalidMember() {
+		val owner = createUser("Owner")
+		val project = projectController.createProject(
+			"testProject",
+			owner.userId
+		)
+
+		Assertions.assertThrows(Exception::class.java) {
+			projectController.createMember(
+				project.projectId,
+				UUID.randomUUID()
+			)
+		}
+
+		val response = projectController.getProject(project.projectId)
+		Assertions.assertEquals(1, response!!.getMembers().size)
+	}
+
+	@Test
+	fun testGetMember() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
+		val project = projectController.createProject(
+			"testProject",
+			owner.userId
+		)
+		val newMember = projectController.createMember(
+			project.projectId,
+			user.userId
+		)
 		val memberResponse = projectController.getMember(
-			response.getId(),
+			project.projectId,
 			newMember.memberId
 		)
 		Assertions.assertNotNull(memberResponse)
 		Assertions.assertEquals("LoginMember", memberResponse!!.login)
 		Assertions.assertEquals("Member", memberResponse.name)
 		Assertions.assertEquals(user.userId, memberResponse.userId)
-		Assertions.assertEquals(response.getId(), memberResponse.projectId)
+		Assertions.assertEquals(project.projectId, memberResponse.projectId)
+	}
 
+	@Test
+	fun testGetNonExistentMember() {
+		val owner = createUser("Owner")
+		val project = projectController.createProject(
+			"testProject",
+			owner.userId
+		)
 		val memberNullResponse = projectController.getMember(
-			response.getId(),
+			project.projectId,
 			UUID.randomUUID()
 		)
 		Assertions.assertNull(memberNullResponse)
+	}
+
+	@Test
+	@Disabled
+	fun testGetAllProjects() {
+		val owner = createUser("Owner")
+
+		val projects = projectController.getAllProjects()
+		Assertions.assertEquals(0, projects.size)
+
+		projectController.createProject(
+			"testProject1",
+			owner.userId
+		)
+		projectController.createProject(
+			"testProject2",
+			owner.userId
+		)
+
+		Thread.sleep(500)
+
+		val newProjects = projectController.getAllProjects()
+		println(newProjects)
+		Assertions.assertEquals(2, newProjects.size)
+		Assertions.assertEquals("testProject1", newProjects[0].name)
+		Assertions.assertEquals("testProject2", newProjects[1].name)
+	}
+
+	@Test
+	@Disabled
+	fun testGetProjectWithAllStatuses() {
+		val owner = createUser("Owner")
+		val project = projectController.createProject(
+			"testProject",
+			owner.userId
+		)
+
+		Thread.sleep(500)
+
+		val response = projectController.getProjectWithAllStatuses(project.projectId)
+		Assertions.assertNotNull(response)
+		Assertions.assertEquals("testProject", response.name)
+		Assertions.assertEquals(1, response.statuses.size)
+		Assertions.assertEquals("CREATED", response.statuses[0].name)
 	}
 
 	private fun createUser(name: String) : UserCreatedEvent {
