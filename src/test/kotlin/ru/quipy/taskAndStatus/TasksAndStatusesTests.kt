6acbf91a1f1de36101f652caa416252f68c24782
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-11-15 01:26:04 +0300

    test: refactor task tests

--- src/test/kotlin/ru/quipy/taskAndStatus/TasksAndStatusesTests.kt
+++ src/test/kotlin/ru/quipy/taskAndStatus/TasksAndStatusesTests.kt
@@ -27,7 +27,7 @@ class TasksAndStatusesTests {
 	private lateinit var taskController: TaskStatusAndTasksController
 
 	@Test
-	fun tasksAndStatusesHappyPath() {
+	fun testDefaultStatusCreation() {
 		val owner = createUser("Owner")
 		val user = createUser("Member")
 
@@ -43,79 +43,166 @@ class TasksAndStatusesTests {
 		Assertions.assertEquals(StatusColor.GREEN, taskAggregate.getStatuses()[0].color)
 		Assertions.assertEquals(1, taskAggregate.getStatuses()[0].position)
 		Assertions.assertEquals(0, taskAggregate.getTasks().size)
+	}
 
-		val defStatus = taskController.getTaskStatus(taskAggregate.getId(), taskAggregate.getStatuses()[0].id)
-		Assertions.assertNotNull(defStatus)
-		Assertions.assertEquals("CREATED", defStatus!!.name)
-		Assertions.assertEquals(StatusColor.GREEN, defStatus.color)
-		Assertions.assertEquals(1, defStatus.position)
+	@Test
+	fun testGetTaskStatus() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
+
+		val project = createProjectWithMember(owner.userId, user.userId)
+
+		val taskAggregate = taskController.getTaskStatusesAndTasks(project!!.getStatusesAndTasksAggregateId())
+		val status = taskController.getTaskStatus(taskAggregate!!.getId(), taskAggregate.getStatuses()[0].id)
+		Assertions.assertNotNull(status)
+		Assertions.assertEquals("CREATED", status!!.name)
+		Assertions.assertEquals(StatusColor.GREEN, status.color)
+		Assertions.assertEquals(1, status.position)
+	}
+
+	@Test
+	fun testNewStatusCreation() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
+
+		val project = createProjectWithMember(owner.userId, user.userId)
 
 		val newStatus = taskController.createTaskStatus(
-			taskAggregate.getId(),
+			project!!.getStatusesAndTasksAggregateId(),
 			"Completed",
 			"ORANGE",
 			null
 		)
-		val newStatusAgg = taskController.getTaskStatus(taskAggregate.getId(), newStatus.statusId)
+		val newStatusAgg = taskController.getTaskStatus(project.getStatusesAndTasksAggregateId(), newStatus.statusId)
 		Assertions.assertNotNull(newStatusAgg)
 		Assertions.assertEquals("Completed", newStatusAgg!!.name)
 		Assertions.assertEquals(StatusColor.ORANGE, newStatusAgg.color)
 		Assertions.assertEquals(2, newStatusAgg.position)
+	}
+
+	@Test
+	fun testTaskCreation() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
+
+		val project = createProjectWithMember(owner.userId, user.userId)
 
 		val task = taskController.createTask(
-			taskAggregate.getId(),
-			newStatus.statusId,
+			project!!.getStatusesAndTasksAggregateId(),
+			project.getStatusesAndTasksAggregateId(),
 			"Task",
 			"Description1"
 		)
-		val taskAgg = taskController.getTask(taskAggregate.getId(), task.taskId)
+		val taskAgg = taskController.getTask(project.getStatusesAndTasksAggregateId(), task.taskId)
 		Assertions.assertNotNull(taskAgg)
 		Assertions.assertEquals("Task", taskAgg!!.name)
 		Assertions.assertEquals("Description1", taskAgg.description)
 		Assertions.assertEquals(0, taskAgg.assignees.size)
-		Assertions.assertEquals(newStatus.statusId, taskAgg.statusId)
+		Assertions.assertEquals(project.getStatusesAndTasksAggregateId(), taskAgg.statusId)
+	}
 
-		Assertions.assertThrows(
-			IllegalStateException::class.java
-		) {
-			taskController.deleteTaskStatus(taskAggregate.getId(), newStatus.statusId)
-		}
+	@Test
+	fun testTaskStatusChange() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
 
-		val taskNewStatus = taskController.changeStatusForTask(
-			taskAggregate.getId(),
-			task.taskId,
-			defStatus.id
+		val project = createProjectWithMember(owner.userId, user.userId)
+
+		val newStatus = taskController.createTaskStatus(
+			project!!.getStatusesAndTasksAggregateId(),
+			"Completed",
+			"ORANGE",
+			null
 		)
-		val taskAggNewStatus = taskController.getTask(taskAggregate.getId(), taskNewStatus.taskId)
+
+		val task = taskController.createTask(
+			project.getStatusesAndTasksAggregateId(),
+			newStatus.statusId,
+			"Task",
+			"Description1"
+		)
+
+		val taskAggregate = taskController.getTaskStatusesAndTasks(project.getStatusesAndTasksAggregateId())
+		val defStatus = taskController.getTaskStatus(taskAggregate!!.getId(), taskAggregate.getStatuses()[0].id)
+		val taskNewStatus = taskController.changeStatusForTask(
+			project.getStatusesAndTasksAggregateId(),
+			task.taskId,
+			defStatus!!.id
+		)
+		val taskAggNewStatus = taskController.getTask(project.getStatusesAndTasksAggregateId(), taskNewStatus.taskId)
 		Assertions.assertNotNull(taskAggNewStatus)
 		Assertions.assertEquals("Task", taskAggNewStatus!!.name)
 		Assertions.assertEquals(defStatus.id, taskAggNewStatus.statusId)
+	}
+
+	@Test
+	fun testTaskNameAndDescriptionUpdate() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
+
+		val project = createProjectWithMember(owner.userId, user.userId)
+
+		val newStatus = taskController.createTaskStatus(
+			project!!.getStatusesAndTasksAggregateId(),
+			"Completed",
+			"ORANGE",
+			null
+		)
+
+		val task = taskController.createTask(
+			project.getStatusesAndTasksAggregateId(),
+			newStatus.statusId,
+			"Task",
+			"Description1"
+		)
 
 		val taskNewNameDescription = taskController.updateTask(
-			taskAggregate.getId(),
-			taskAgg.id,
+			project.getStatusesAndTasksAggregateId(),
+			task.taskId,
 			"newNameTask",
 			"newDescription"
 		)
-		val taskAggNewNameDescription = taskController.getTask(taskAggregate.getId(), taskNewNameDescription.taskId)
+		val taskAggNewNameDescription = taskController.getTask(project.getStatusesAndTasksAggregateId(), taskNewNameDescription.taskId)
 		Assertions.assertNotNull(taskAggNewNameDescription)
 		Assertions.assertEquals("newNameTask", taskAggNewNameDescription!!.name)
 		Assertions.assertEquals("newDescription", taskAggNewNameDescription.description)
-		Assertions.assertEquals(defStatus.id, taskAggNewStatus.statusId)
-
-		taskController.deleteTaskStatus(taskAggregate.getId(), newStatus.statusId)
-		val agg = taskController.getTaskStatusesAndTasks(taskAggregate.getId())
-		Assertions.assertNotNull(agg)
-		Assertions.assertEquals(1, agg!!.getStatuses().size)
-		Assertions.assertEquals("CREATED", agg.getStatuses()[0].name)
-		Assertions.assertEquals(StatusColor.GREEN, agg.getStatuses()[0].color)
-		Assertions.assertEquals(1, agg.getStatuses()[0].position)
-		Assertions.assertEquals(1, agg.getTasks().size)
 	}
 
+	@Test
+	fun testAddAssigneeToTask() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
+
+		val project = createProjectWithMember(owner.userId, user.userId)
+
+		val task = taskController.createTask(
+			project!!.getStatusesAndTasksAggregateId(),
+			project.getStatusesAndTasksAggregateId(),
+			"Task",
+			"Description1"
+		)
+
+		val taskWithAssignee = taskController.addAssigneeForTask(
+			project.getStatusesAndTasksAggregateId(),
+			task.taskId,
+			user.userId
+		)
+		val taskAggWithAssignee = taskController.getTask(project.getStatusesAndTasksAggregateId(), taskWithAssignee!!.taskId)
+		Assertions.assertNotNull(taskAggWithAssignee)
+		Assertions.assertEquals(1, taskAggWithAssignee!!.assignees.size)
+		Assertions.assertEquals(user.userId, taskAggWithAssignee.assignees[0])
+
+		Assertions.assertThrows(IllegalArgumentException::class.java) {
+			taskController.addAssigneeForTask(
+				project.getStatusesAndTasksAggregateId(),
+				task.taskId,
+				user.userId
+			)
+		}
+	}
 
 	@Test
-	fun statusesSwapPositionsHappyPath() {
+	fun testStatusesSwapPositions() {
 		val owner = createUser("Owner")
 		val user = createUser("Member")
 		val project = createProjectWithMember(owner.userId, user.userId)
@@ -154,6 +241,14 @@ class TasksAndStatusesTests {
 		validateStatus(taskAggregate.getId(), secondId, secondStatus, 2)
 		validateStatus(taskAggregate.getId(), thirdId, thirdStatus, 3)
 
+		Assertions.assertThrows(IllegalArgumentException::class.java) {
+			taskController.changeTaskStatusPosition(
+				taskAggregate.getId(),
+				secondId,
+				10
+			)
+		}
+
 		taskController.changeTaskStatusPosition(
 			taskAggregate.getId(),
 			thirdId,
@@ -163,15 +258,45 @@ class TasksAndStatusesTests {
 		validateStatus(taskAggregate.getId(), firstId, firstStatus, 4)
 		validateStatus(taskAggregate.getId(), secondId, secondStatus, 3)
 		validateStatus(taskAggregate.getId(), thirdId, thirdStatus, 1)
+	}
 
-		taskController.deleteTaskStatus(taskAggregate.getId(), thirdId)
+	@Test
+	fun testStatusDeletion() {
+		val owner = createUser("Owner")
+		val user = createUser("Member")
+		val project = createProjectWithMember(owner.userId, user.userId)
+
+		val zeroId = taskController
+			.getTaskStatusesAndTasks(project!!.getStatusesAndTasksAggregateId())!!
+			.getStatuses()[0].id
+
+		val firstId = createStatus(
+			project.getStatusesAndTasksAggregateId(),
+			firstStatus
+		).statusId
+		val secondId = createStatus(
+			project.getStatusesAndTasksAggregateId(),
+			secondStatus
+		).statusId
+
+		val taskAggregate = taskController.getTaskStatusesAndTasks(project.getStatusesAndTasksAggregateId())
+
+		taskController.deleteTaskStatus(taskAggregate!!.getId(), firstId)
 		validateStatus(taskAggregate.getId(), zeroId, zeroStatus, 1)
-		validateStatus(taskAggregate.getId(), firstId, firstStatus, 3)
 		validateStatus(taskAggregate.getId(), secondId, secondStatus, 2)
 
-		taskController.deleteTaskStatus(taskAggregate.getId(), firstId)
-		validateStatus(taskAggregate.getId(), zeroId, zeroStatus, 1)
-		validateStatus(taskAggregate.getId(), secondId, secondStatus, 2)
+		taskController.createTask(
+			taskAggregate.getId(),
+			zeroId,
+			"Task",
+			"Description1"
+		)
+
+		Assertions.assertThrows(
+			IllegalStateException::class.java
+		) {
+			taskController.deleteTaskStatus(taskAggregate.getId(), zeroId)
+		}
 	}
 
 	private fun validateStatus(aggId: UUID, actualId: UUID, expectedStatus: TaskStatusEntity, expectedPosition: Int) {
@@ -203,7 +328,7 @@ class TasksAndStatusesTests {
 			"testProject",
 			ownerId
 		)
-		val newMember = projectController.createMember(
+		projectController.createMember(
 			response.projectId,
 			userId
 		)
